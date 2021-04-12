3c261165132657b4951b7678481c5cf1fc478ab8
amylopata <alopata@andrew.cmu.edu>
2021-04-12 13:33:01 -0500

    Adding background behind each path

--- accessibility.js
+++ accessibility.js
@@ -13,7 +13,7 @@ function loadpath(layerfile, color = '#D41159', dash = "4,4") {
 		},
 		/* Add a line behind each path as a background for emphasis */
 		onEachFeature : function (feature, layer) {
-			background = L.polyline(feature.getLatLngs());
+			background = geometryToLayer(feature);
 			background.addTo(map);
 		}
 	});
